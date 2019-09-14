0586a55a74903d8955510b11fc330d50b02da918
Philsun01 <philsun@gmail.com>
2019-09-14 11:16:54 -0700

    the app gets broke

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const express = require('express');
-const db = require('./db');
+//const db = require('./db');
 const app = express();
 const path = require('path');
 
 app.get('/', (req, res, next)=> res.sendFile(path.join(__dirname, 'index.html')));
 
-app.listen(3000, ()=> console.log('Listening on Port 3000'));
+app.get('/api/users', (req, res, next) => {
+  res.send('This it the USERS URL');
+  /*try {
+    const users = await db.findAllUsers();
+    res.send(users);
+  }
+  catch(ex) {
+  }*/
+});
+​/*
+app.get('/api/departments', (req, res, next) => {
+  res.send('This is the department URL')
+  /*try {
+    const departments = await db.findAllDepartments();
+    res.send(departments);
+  }
+  catch(ex) {
+  }*/
+});
+
+/*db.syncAndSeed()
+  .then(() => app.listen(3000, ()=> console.log('We are listening on port 3000')));*/
+  app.listen(3000, ()=> console.log('We are listening on port 3000'))
