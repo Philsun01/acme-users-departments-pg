3cf51c62865de582a621b8e743e3f9edf2c48faf
Philsun01 <philsun@gmail.com>
2019-09-14 10:44:20 -0700

    started DB file

--- db.js
+++ db.js
@@ -1,1 +1,12 @@
-//Our DB JS file
+const pg = require('pg');
+const {Client} = pg;
+
+const uuid = require('uuid');
+const client = new Client('postgres://localhost/acme_users_department');
+
+client.connect();
+
+const SQL = `
+
+
+`
--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-//This is a server file
+const express = require('express');
+const db = require('./db');
+const app = express();
+const path = require('path');
+
+app.get('/', (req, res, next)=> res.sendFile(path.join(__dirname, 'index.html')));
+
+app.listen(3000, ()=> console.log('Listening on Port 3000'));
