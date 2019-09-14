b1077f6459bb1c6a0cb5ef8f14e602a73ddb8adf
Philsun01 <philsun@gmail.com>
2019-09-14 11:03:32 -0700

    DB file written

--- db.js
+++ db.js
@@ -5,8 +5,50 @@ const uuid = require('uuid');
 const client = new Client('postgres://localhost/acme_users_department');
 
 client.connect();
+//user has id, name, department_id and department has id, name
 
 const SQL = `
+  DROP TABLE IF EXISTS users;
+  DROP TABLE IF EXISTS departments;
 
+  CREATE TABLE departments(
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
+  );
+
+  CREATE TABLE users(
+    if UUID PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
+    category_id UUID REFERENCE departments(id)
+  );
+
+  INSERT INTO departments(id, name)
+  values('12345','Accounting');
+  INSERT INTO departments(id, name)
+  values('12346','Marketing');
+
+  INSERT INTO users(id, name, department_id)
+  values('5678','Howard');
+  INSERT INTO users(id, name, department_id)
+  values('7890','Jimmy');
 
 `
+const syncAndSeed = async() => {
+  await client.query(SQL);
+};
+
+const findAllUsers = async() => {
+  const response = await client.query('SELECT * FROM users;');
+  return response.rows;
+}
+
+const findAllDepartments = async() => {
+  const response = await client.query('SELECT * FROM departments;');
+  return response.rows;
+}
+
+module.exports = {
+  syncAndSeed,
+  findAllUsers,
+  findAllDepartments
+}
